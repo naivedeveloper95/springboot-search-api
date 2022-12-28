fe43816a3e1c60975d40968bba41dc51178dfb34
Satyam Suman <findsatyam95@gmail.com>
2022-12-29 00:00:35 +0530

    Initial Commit

---
+++ src/test/java/com/practicespringboot/springbootsearchrestapi/SpringbootSearchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicespringboot.springbootsearchrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSearchRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
